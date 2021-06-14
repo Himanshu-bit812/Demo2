a9c9aa31a333e18b7ae183c8499537c63d9e6756
Himanshu-bit812 <85785771+Himanshu-bit812@users.noreply.github.com>
2021-06-14 17:28:35 +0530

    Add files via upload

---
+++ Java/Himanshu Tomar - 1922026/DemoTest.java
@@ -0,0 +1,8 @@
+class DemoTest{
+	public static void main(String[] args)
+	{
+	String Name="Himanshu";
+	System.out.println("Before Using ToupperCase: "+Name);
+	System.out.println("After Using ToUpperCase: "+Name.toUpperCase());
+	}
+}
